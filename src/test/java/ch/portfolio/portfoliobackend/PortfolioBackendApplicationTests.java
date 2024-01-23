af7b1aa9678a9930c750a139cae6091b5458b7d1
Odermatt Marco <Marco.Odermatt@swisscom.com>
2024-01-23 14:47:17 +0100

    init

---
+++ src/test/java/ch/portfolio/portfoliobackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.portfolio.portfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
