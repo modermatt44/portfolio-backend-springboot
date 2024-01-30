291e8fb7b4a20f66052172b02b8848d26c5b6675
Odermatt Marco <Marco.Odermatt@swisscom.com>
2024-01-30 08:13:54 +0100

    added form login and env variables for usename/password

--- src/main/java/ch/portfolio/portfoliobackend/config/SecurityConfig.java
+++ src/main/java/ch/portfolio/portfoliobackend/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                         .requestMatchers("/post_project").authenticated()
                         .anyRequest().permitAll()
                 )
+                .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
     }
