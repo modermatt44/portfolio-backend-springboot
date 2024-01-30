33f1f979027cdb0d73c3c98dd671e1fb1cf7217f
Odermatt Marco <Marco.Odermatt@swisscom.com>
2024-01-30 08:28:38 +0100

    added custom user

--- src/main/java/ch/portfolio/portfoliobackend/config/SecurityConfig.java
+++ src/main/java/ch/portfolio/portfoliobackend/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package ch.portfolio.portfoliobackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,6 +18,31 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.builder()
+                        .passwordEncoder(passwordEncoder()::encode)
+                        .username(username)
+                        .password(password)
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     // Security filter chain
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
