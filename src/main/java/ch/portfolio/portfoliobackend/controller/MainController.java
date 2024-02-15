df6dd19f7b42557f8b25d7a0e3a1c8afd52f4365
TAAODMA2 <marco.odermatt@swisscom.com>
2024-02-15 15:50:32 +0100

    removed basic auth

--- src/main/java/ch/portfolio/portfoliobackend/controller/MainController.java
+++ src/main/java/ch/portfolio/portfoliobackend/controller/MainController.java
@@ -1,5 +1,7 @@
 package ch.portfolio.portfoliobackend.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
