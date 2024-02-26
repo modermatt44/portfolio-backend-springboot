94c7c4b67bd617624642e20e978da34a6c84a2bc
TAAODMA2 <marco.odermatt@swisscom.com>
2024-02-26 15:38:30 +0100

    added error page for 403, changed logout function on admin page, added oauth validation inside spring

--- src/main/java/ch/portfolio/portfoliobackend/controller/AdminController.java
+++ src/main/java/ch/portfolio/portfoliobackend/controller/AdminController.java
@@ -1,34 +1,36 @@
 package ch.portfolio.portfoliobackend.controller;
 
-import ch.portfolio.portfoliobackend.model.Language;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import ch.portfolio.portfoliobackend.repository.LanguageRepository;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Objects;
 
 @Controller
 public class AdminController {
 
-    @Autowired
-    public LanguageRepository languageRepository;
+    @Value("${oauthAttribute}")
+    private String oauthAttribute;
+
+    @Value("${oauthAttributeValue}")
+    private String oauthAttributeValue;
+
+    public boolean validateUser(@AuthenticationPrincipal OAuth2User user) {
+      return Objects.equals(user.getAttribute(oauthAttribute), oauthAttributeValue);
+    }
 
     @GetMapping("/admin")
-    public String showAdmin(Model model, String title, @AuthenticationPrincipal OAuth2User user){
+    public String showAdmin(Model model, @AuthenticationPrincipal OAuth2User user){
+
+        if (!validateUser(user)) {
+            return "error-403";
+        }
 
         model.addAttribute("username", "Hello " + user.getAttribute("name"));
 
         return "AdminPage";
-    }
-
-    @DeleteMapping("/delete_language_ui")
-    public String deleteLanguage(@ModelAttribute("language") String title) {
-        // Delete a language object from the database by title
-        languageRepository.deleteByTitle(title);
-        return "redirect:/admin";
     }
 }
